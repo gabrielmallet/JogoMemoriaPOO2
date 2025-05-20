31faeb9b000cb094a727e5823d9c66c88a66afbf
gabrielmallet <bielmallet@hotmail.com>
2025-05-20 11:01:34 -0300

    Commit Inicial

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Classe que executa o metodo principal da Interface gráfica.
+ * @author Gabriel
+ * @version 0.1
+ * @see InterfaceGrafica
+ */
+public class Main {
+    public static void main(String[] args) {
+        new InterfaceGrafica();
+    }
+}
